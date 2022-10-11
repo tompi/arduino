da2e2e8dbe61d2c27e21bad5546b1360e97bd121
Thomas Haukland <thomas.haukland@gmail.com>
2022-10-11 22:07:54 +0200

    reading direct from i2c

--- sketch_pcf857/sketch_pcf857.ino
+++ sketch_pcf857/sketch_pcf857.ino
@@ -1,56 +1,51 @@
-#include <Wire.h>
-#include <Adafruit_PCF8574.h>
-Adafruit_PCF8574 pcf;
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+
+
+Adafruit_I2CDevice *i2c_dev;
 
 const int LED_COUNT = 4;
-int leds[LED_COUNT] = {D7,D8,D14,D15};
+int leds[LED_COUNT] = {D7,D8,D15,D14};
+bool led_status[LED_COUNT] = {0,0,0,0};
+
+uint8_t _readbuf = 0, _writebuf = 0;
 
 void setup() {
 
- for (int i=0; i<LED_COUNT; i++)
- {
-  pinMode(leds[i], OUTPUT);
-  digitalWrite(leds[i], HIGH);
- }
-  delay(1000);   
-  //while (!Serial) { delay(10); }
-  //Serial.begin(115200);
-  //Serial.println("Adafruit PCF8574 button read test");
+  delay(100);   
 
-
-  
-  
-  //setSDA(D0);
-  //setSCL(D1); 
-
-  if (!pcf.begin(0x20, &Wire)) {
-    //Serial.println("Couldn't find PCF8574");
-    while (1);
+  i2c_dev = new Adafruit_I2CDevice(0x20, &Wire);
+  i2c_dev->begin();
+  for (int i=0; i<LED_COUNT; i++)
+  {
+    pinMode(leds[i], OUTPUT);
   }
+  
+  // turn leds on and off to see they are working
+  toggle_leds(0b00001111);
+  delay(2000);
+  toggle_leds(0);
 
-    pcf.pinMode(0,  OUTPUT);
-    pcf.pinMode(1,  INPUT_PULLUP);
-    pcf.pinMode(2,  OUTPUT);
-    pcf.pinMode(3,  INPUT_PULLUP);
 
-  for (uint8_t p=4; p<8; p++) {
-    pcf.pinMode(p, INPUT_PULLUP);
-  }
+  // set pin 0 and 2 to output, rest to input
+  // 11111010
+  //_writebuf = ^0 ~1 ~(1<<2);
+  _writebuf = 0b11111010;
+
+  i2c_dev->write(&_writebuf, 1);
 }
 
 void loop() {
-  //for (uint8_t p=0; p<8; p++) {
-    if (!pcf.digitalRead(0)) {
-      //Serial.print("Button on GPIO #");
-      //Serial.print(p);
-      //Serial.println(" pressed!");
-      // put your main code here, to run repeatedly:
-      // digitalWrite(D14, HIGH);   // turn the LED on (HIGH is the voltage level)
-    }
-    else
-    {
-      //digitalWrite(D14, LOW);      
-    }
-  //} 
+  i2c_dev->read(&_readbuf, 1);
+
+  toggle_leds(_readbuf);
+
   delay(10); // a short debounce delay
+}
+
+void toggle_leds(uint8_t leds_status) {
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    digitalWrite(leds[i], ((1<<i) & leds_status) ? HIGH : LOW);
+  }
 }
